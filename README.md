e23ab7fd45023bfe330fe58c33e5ac81d70ea41f
XuRuihan <1700017793@pku.edu.cn>
2020-12-16 01:13:06 +0800

    Finished review

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Mahjong
+
+当前任务完成度：
+
+- [x] 复盘（获得state）
+- [ ] 获取行动空间（action space）
+- [ ] 算番（reward）
+- [ ] 选择行动（回归/搜索/RL）
+
+## bot.py
+
+注：
+
+1. 目前不使用【长时运行】。但是我把接口写成两个部分方便修改为长时运行
+
+   ```python
+   reviewOneStep()  # 复盘一步
+   review()         # 复盘全部
+   ```
+
+   review 内部循环调用 reviewOneStep 实现全部复盘。只用 reviewOneStep 就可以使用长时运行。
+
+2. 目前从input.json读取输入，提交时改成从input()读取
+
+3. 需要安装算番库 https://github.com/ailab-pku/Chinese-Standard-Mahjong
+
+## const.py
+
+存放常数
+
+## inputX.json
+
+样例输入文件，有3个，包含吃、碰、补杠等情形，用于debug
--- bot.py
+++ bot.py
@@ -1,13 +1,11 @@
 import json
 import random
-from MahjongGB import MahjongFanCalculator
+import torch
+from const import *  # 导入常量
+from MahjongGB import MahjongFanCalculator  # 算番
 """某个运行实例的输入。每行代表一个step
 {"requests":["0 0 3","1 0 0 0 0 B2 B1 B1 J2 J3 W2 T5 W2 F1 T3 F4 T8 F2","2 F2","3 0 PLAY F1","3 1 DRAW","3 1 PLAY T4"],"responses":["PASS","PASS","PLAY F1","PASS","PASS"]}
 """
-
-# 定义 request 常量
-SELF_DRAW = "2"
-OTHERS_TURN = "3"
 
 
 class Mahjong():
@@ -16,151 +14,186 @@ class Mahjong():
 
     def reset(self):  # 用于后期训练 RL 模型的接口
         self.turnID = -1
-        self.menFeng = 0
+        self.menFeng = 0  # playerID 就是门风
         self.quanFeng = 0
 
         self.pack = []  # ((packType, tileCode, data), ...),
-        self.hand = []
-        # self.gang = []  # (牌代码, 1 上家供牌 / 2 对家供牌 / 3 下家供牌)
-        # self.peng = []  # 同上
-        # self.chi = []  # (中间牌代码, 第 1 / 2 / 3 张是上家供牌)
+        self.hand = [0] * TILE_NUM
 
-        self.play = {}  # 记录已经打出的牌
-        self.left = 88  # 初始牌墙中的牌数
+        self.unknown = [4] * TILE_NUM  # 记录已经打出的牌
+        self.left = 21  # 初始牌墙中的牌数
 
-    def calculateFan(self, state):
+    # 字符串表示转换成向量表示
+    def deck2vector(self):
+        # TODO
+        pass
+
+    # 向量表示转换成字符串表示
+    def vector2deck(self):
+        # TODO
+        pass
+
+    def calculateFan(self, tileCode, isZIMO, isGANG):
+        handcode = []
+        for idx, num in enumerate(self.hand):
+            handcode += [idx2tile[idx]] * num
+        isJUEZHANG = (self.unknown[tile2idx[tileCode]] == 1)
+        isLAST = (self.left == 0)
+
         try:
             fans = MahjongFanCalculator(
                 self.pack,  # 明牌
-                self.hand,  # 暗牌
-                state["tileCode"],  # 和的那张牌
+                handcode,  # 暗牌
+                tileCode,  # 和的那张牌
                 0,  # 补花数，在本模型中应当设为 0
-                state["SELF_DRAW"],  # 自摸
-                state["isJUEZHANG"],  # 绝张
-                state["isGANG"],  # 抢杠和/杠上开花
-                state["isLAST"],  # 牌墙最后一张，妙手回春/海底捞月
+                isZIMO,  # 自摸
+                isJUEZHANG,  # 绝张
+                isGANG,  # 抢杠和/杠上开花
+                isLAST,  # 牌墙最后一张，妙手回春/海底捞月
                 self.menFeng,  # 门风
                 self.quanFeng,  # 圈风
             )
             totalFan = sum(fan[0] for fan in fans)  # fan[0] 为番数，fan[1] 为番形名称
             return totalFan
         except Exception as e:
-            return 0  # 表示没有赢，因此番数为 0
+            return 0  # 没有和，番数为 0
+
+    def tileFrom(self, playerID):
+        # 0 - 本家，1 - 上家，2 - 对家，3 - 下家
+        return (self.menFeng - playerID) % 4
+
+    def reviewOneStep(self, req, last_req):
+        '''一步复盘
+        Args:
+            req - 本回合 botzone 发给所有玩家的信息
+            last_req - 上回合 botzone 发给所有玩家的信息
+        Return:
+            无返回值
+        '''
+        curr_idx = tile2idx.get(req[-1], None)
+        last_idx = tile2idx.get(last_req[-1], None)
+        if req[0] == SELF_DRAW:  # 当前为己方玩家摸牌
+            self.left -= 1
+            self.hand[curr_idx] += 1
+            self.unknown[curr_idx] -= 1
+
+        elif req[0] == OTHERS_TURN:  # 当前不为己方玩家摸牌
+            playerID = int(req[1])  # 当前玩家编号
+            if req[2] == DRAW:  # 摸牌，此时玩家编号不可能是自己
+                pass
+
+            elif req[2] == PLAY:  # 打牌
+                if playerID == self.menFeng:
+                    self.hand[curr_idx] -= 1
+                else:
+                    self.unknown[curr_idx] -= 1
+
+            elif req[2] == PENG:  # 碰
+                if playerID == self.menFeng:
+                    self.hand[last_idx] -= 2
+                    self.hand[curr_idx] -= 1
+                    self.pack.append(
+                        (PENG, idx2tile[last_idx], self.tileFrom(playerID)))
+                else:
+                    self.unknown[last_idx] -= 2  # 上回合打出的牌
+                    self.unknown[curr_idx] -= 1  # 碰之后打出的牌
+
+            elif req[2] == CHI:  # 吃
+                mididx = tile2idx[req[-2]]
+                chi = {mididx - 1, mididx, mididx + 1}
+                chi.remove(last_idx)
+                if playerID == self.menFeng:
+                    for idx in chi:
+                        self.hand[idx] -= 1
+                    self.hand[curr_idx] -= 1
+                    self.pack.append(
+                        (CHI, idx2tile[mididx], last_idx - mididx + 2))
+                else:
+                    for idx in chi:
+                        self.unknown[idx] -= 1  # 吃的另外两张牌
+                    self.unknown[curr_idx] -= 1  # 吃之后打出的牌
+
+            elif req[2] == GANG:  # 杠
+                if playerID == self.menFeng:  # 如果是自己，那么明杠暗杠都会少手牌
+                    self.hand[last_idx] -= 3
+                    self.pack.append(
+                        (GANG, idx2tile[last_idx], self.tileFrom(playerID)))
+                # 他人明杠可以用于计算绝张
+                elif last_req[-1] != DRAW and last_req[0] != SELF_DRAW:
+                    self.unknown[last_idx] -= 3
+
+            elif req[2] == BUGANG:  # 补杠
+                if playerID == self.menFeng:
+                    self.hand[curr_idx] -= 1
+                    self.pack.append(
+                        (GANG, idx2tile[last_idx], self.tileFrom(playerID)))
+                else:
+                    self.unknown[curr_idx] -= 1
 
     def review(self):
-        '''根据输入的 request 复盘
+        '''完整复盘
         Args:
-            命令行输入
+            （命令行输入）
         Return:
-            字符串列表，为当前输入的 request 的切分
+            req - 当前输入的 request 切分的字符串列表
+            last_req - 上一次输入的 request 切分的字符串列表
         '''
-        inputJson = json.loads(input())
-        requests, responses = inputJson["requests"], inputJson["responses"]
-        self.turnID = len(responses)
-        if self.turnID == 0:  # 记录 playerID 和 quan （playerID和门风是否相同？）
-            _, self.menFeng, self.quanFeng = tuple(
-                map(int, requests[0].split()))
-        elif self.turnID == 1:  # 起始手牌，只有在跨回合运行的bot中才能用上
-            self.hand = requests[1].split()[5:18]
-        else:
-            self.hand = requests[1].split()[5:18]
+        # inputJson = json.loads(input())
+        with open("input1.json", "r", encoding="utf8") as f:
+            inputJson = json.load(f)
+        requests = inputJson["requests"]
+        self.turnID = len(requests) - 1
+
+        last_req = requests[0].split()  # 用于记录上一步输入内容
+        if self.turnID >= 2:
+            # 记录 menFeng 和 quanFeng（playerID 就是门风）
+            _, self.menFeng, self.quanFeng = list(map(int,
+                                                      requests[0].split()))
+
+            # 起始手牌，只有在跨回合运行的bot中才能用上
+            handcode = requests[1].split()[5:18]
+            for tile in handcode:
+                self.hand[tile2idx[tile]] += 1
+                self.unknown[tile2idx[tile]] -= 1
+
+            # 从第2回合开始，根据每一步输入输出恢复状态
             for i in range(2, self.turnID):
                 req = requests[i].split()
-                res = responses[i].split()
-                if req[0] == SELF_DRAW:  # 摸牌
-                    self.left -= 1
-                    self.hand.append(req[1])
-                    self.hand.remove(res[1])
-                elif req[0] == OTHERS_TURN:
-                    if req[2] == "PLAY":
-                        # 记录打出的牌
-                        self.play[req[3]] = self.play.get(req[3], 0) + 1
-                    elif req[2] == "DRAW":
-                        self.left -= 1
+                self.reviewOneStep(req, last_req)
+                last_req = req
+
         req = requests[-1].split()  # 当前回合输入
         return req
 
-    def getState(self):  # 读取数据并转换成当前状态
-        '''获取当前对局状态
-        Args:
-            命令行输入
-        Return:
-            字典，包含当前 request 解析后的状态
-        '''
-        req = self.review()  # 复盘之前的状态
-        # 以下获取当前的状态
-        state = {
-            "SELF_DRAW": False,  # 当前是否摸牌
-            "OTHERS_PLAY": False,  # 当前是否有其他人出牌（需要判断碰/杠/吃/和）
-            "tileCode": "",  # 牌形
-            "isJUEZHANG": False,  # 绝张
-            "isGANG": False,  # 杠
-            "isLAST": False,  # 最后一张
-        }
-        if req[0] == SELF_DRAW:
-            state["SELF_DRAW"] = True
-            state["tileCode"] == req[1]
-            state["isZIMO"] == True
-            state["isJUEZHANG"] = (self.play.get(req[1]) == 3)
-            state["isLAST"] = (self.left == 1)
-        elif req[0] == OTHERS_TURN:
-            if req[2] == "PLAY" and req[1] != self.menFeng:
-                state["tileCode"] == req[3]
-                state["OTHERS_PLAY"] == True
-        return state
-
-    def getAction(self, state):
+    def getAction(self, req):
         if self.turnID < 2:
-            return "PASS"
-        if state["SELF_DRAW"]:  # 自摸
-            playCard = random.choice(self.hand)
-            return "PLAY " + playCard
-        elif state["OTHERS_PLAY"]:  # 其他家出牌
-            ### TODO
-            return "PASS"
-        else:  # 否则无需采取行动
-            return "PASS"
+            return PASS
+        if req[0] == SELF_DRAW:  # 自摸
+            # 这里需要调整策略
+            # 此时可行的策略为【和/打牌/暗杠/补杠】
+            # isGANG 的判断需要改写
+            fan = self.calculateFan(req[-1], isZIMO=True, isGANG=False)
+            if fan >= QIHU:
+                return HU
+            playtile = random.choice(self.hand)
+            return f"PLAY {playtile}"
 
-    # 合法性检查
-    def checkValid(self, action):
-        # 上回合输入条件 / 本回合操作 / 下回合的输入
-        res = action.split()
-        if res[0] == "PLAY":
-            # PLAY T6
-            # 摸牌 / 打出的牌在手牌中 /
-            assert res[1] in self.hand
-        elif res[0] == "GANG":
-            # GANG T6
-            # 摸牌 / 已有三张，杠 / 摸牌
+        elif req[0] == OTHERS_TURN:  # 其他家出牌
+            playerID = int(req[1])
+            # 此时如果是【他人摸牌/杠（暗杠）/补杠/自己打牌】，则无需任何操作
+            if req[2] == DRAW or req[2] == GANG or req[2] == BUGANG \
+                or (req[2] == PLAY and playerID == self.menFeng):
+                return PASS
 
-            # GANG
-            # 其他玩家打牌 / 已有三张，杠 / 摸牌
-            pass
-        elif res[0] == "BUGANG":
-            # BUGANG T6
-            # 摸牌 / 已经有碰，补杠 / 摸牌
-            pass
-        elif res[0] == "HU":
-            # HU
-            # 摸牌 / 和牌 /
-            # 其他玩家打牌 / 和牌 /
-            pass
-        elif res[0] == "PENG":
-            # PENG T6
-            # 其他玩家打牌 / 已有两张，碰，打出一张 /
-            pass
-        elif res[0] == "CHI":
-            # CHI T2 T5 （T2是顺子的中间牌）
-            # 上位玩家出牌 / 已有两张，吃，打出一张 /
-            pass
-        elif res[0] == "PASS":
-            pass
-        else:
-            raise Exception("Unknown response")
+            # 否则可行的策略为【和/碰/杠/吃（需要判断上家）】
+            else:
+                if self.tileFrom(playerID) == 1:  # 来自上家
+                    pass
+                return PASS
 
     def step(self):
-        currState = self.getState()
-        action = self.getAction(currState)
+        req = self.review()  # 复盘之前的状态
+        action = self.getAction(req)
         print(json.dumps({"response": action}))
 
 
